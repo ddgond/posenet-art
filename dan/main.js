726857142a22190962341cfffa622dd7c64cb29e
Daniel Dangond <dadangond@gmail.com>
2020-10-17 18:23:09 -0400

    Removed logging

--- dan/main.js
+++ dan/main.js
@@ -2,7 +2,6 @@ const clapToSparkleListener = new EventListener('leftWrist', EventType.Collision
   if (data.collider.name === 'rightWrist') {
     for (let i = 0; i < 25; i++) {
       const randomOffset = () => (Math.random() * 100 - 50) / getDistanceRatio();
-      console.log(randomOffset());
       Engine.addAnimatedObject(new Sparkle(keypoint.x + randomOffset(), keypoint.y + randomOffset(), 20, 3000));
       Engine.addAnimatedObject(new Sparkle(data.collider.x + randomOffset(), data.collider.y + randomOffset(), 20, 3000));
     }
